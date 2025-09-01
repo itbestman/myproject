2159c52e5d9143daf8c26aee133fec4a222f4af1
itbestman@gmail.com <itbes@LAPTOP-MRMG24T9>
2025-09-01 10:31:13 +0530

    9/1/2025

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Child } from "./child/child";
+import {Home} from './home/home';
+
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Child, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('myproject');
+  name = 'Manish Kumar Gupta';
+
+
+  namechanged(name: string) {
+  
+    this.name = name;
+  }
 }
